81ad78049da9f772026f26a71960432bbf787acf
Mateusz Rojek <mateuszr2047@gmail.com>
2023-09-26 18:10:28 +0200

    Commit2

--- src/main/java/Class.java
+++ src/main/java/Class.java
@@ -3,6 +3,8 @@ public class Class {
         System.out.println("Uczę");
         System.out.println("się");
         System.out.println("programowania");
+        System.out.println("i");
+        System.out.println("commitowania");
     }
 
 }
